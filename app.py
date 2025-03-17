83034cbbfebcaea97084a8ada758dc3718466bf0
Belek Bagishbekov <billionaires@Beleks-MacBook-Pro.local>
2025-03-16 22:47:49 -0400

    update ecs cluster nmae

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ CORS(app)  # Allow all domains
 
 # Load DB config from environment variables (set by ECS)
 DB_HOST = os.getenv('DB_HOST', 'db.petstore.internal')
-DB_PORT = os.getenv('DB_PORT', '5432')
+DB_PORT = os.getenv('DB_PORT', '3306')
 DB_NAME = os.getenv('DB_NAME', 'petstore')
 DB_USER = os.getenv('DB_USER', 'admin')
 DB_PASSWORD = os.getenv('DB_PASSWORD')  # Must be set in ECS secrets
